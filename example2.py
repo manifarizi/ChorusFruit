b90bb5b035b179914d0ab3abba06a27ce2965d67
mani.f <78233841+manifarizi@users.noreply.github.com>
2022-08-08 02:37:21 +0430

    Update example2.py

--- example2.py
+++ example2.py
@@ -6,7 +6,7 @@ def main(self):
     title = '[A Window]'
     scrx = int(cf.COLS / 2)
     scry = int(cf.LINES / 2)
-    for i in range(scry - 2 - len(litem)):
+    for i in range(scry - 1 - len(litem)):
         litem.append('')
     self.Window(self.make_Style('[back_red][Hi]'), scrx, scry, False, '┌─┐││└─┘', '[back_black]')
     dcc = 0
@@ -14,6 +14,6 @@ def main(self):
 
     for i in litem:
         dcc += 1
-        self.write((cf.COLS // 2) - (scrx // 2) + 2, (cf.LINES / 2) - (scry // 2) + dcc, ' ' + i + ' ' * (icc - len(i)), '[back_red]')
+        self.write((cf.COLS // 2) - (scrx // 2) + 2, (cf.LINES / 2) - (scry // 2) + 1 + dcc, ' ' + i + ' ' * (icc - len(i)), '[back_red]')
     self.write((cf.COLS // 2) - (scrx // 2) + 2, (cf.LINES / 2) - (scry // 2) + 1 + dcc, i + '▃' * (icc - len(i) + 1), '[back_red][fore_black]')
 main(Screen())
