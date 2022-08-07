1e02eed38756ef09f72e4d01f4b02ae526d75da4
mani.f <78233841+manifarizi@users.noreply.github.com>
2022-08-07 19:10:24 +0430

    Made getch cross-platform with py-getch
    pip install py-getch

--- src/ChorusFruit.py
+++ src/ChorusFruit.py
@@ -1,4 +1,4 @@
-from msvcrt import getche, getch
+from getch import getch
 import AnsiList
 import sys
 import os
@@ -10,10 +10,13 @@ def ColsAndLines():
     COLS = os.get_terminal_size()[0] - 0
     LINES = os.get_terminal_size()[1]
 class Screen(object):
+    """Main Class"""
     def reset(self):
+        """Resets Screen"""
         self.clear()
         ColsAndLines()
     def make_color(self, text) -> None:
+        """Replace with color"""
         all_color_list = []
         for i in AnsiList.back_styles:
             all_color_list.append('[back_' + i + ']')
@@ -31,6 +34,7 @@ class Screen(object):
         self.reset()
 
     def write(self, y:int, x:int, string: str, Style: str=None, flush: bool=True) -> None:
+        """Add text to a X and a Y"""
         if not Style == None:
             string = self.make_color('[' + Style + ']' + string)
         if not y == None and not x == None:
@@ -40,10 +44,13 @@ class Screen(object):
         if flush:
             sys.stdout.flush()
     def DOWN(self) -> None:
+        """Go one line Down"""
         print()
     def UP(self) -> None:
+        """Go one line Up"""
         sys.stdout.write("\033[F")
     def box(self, x: int, y: int, lx: int, ly: int, lines: str='┌─┐││└─┘') -> None:
+        """Makes a box"""
         self.write(x, y, lines[0] + lines[1] * (lx - 2) + lines[2], flush=True)
         for i in range(int(ly - 1)):
             y += 1
@@ -51,22 +58,31 @@ class Screen(object):
             self.write(x, y, lines[3] + ' ' * (lx - 2) + lines[4], flush=True)
         self.write(x, y, lines[5] + lines[6] * (lx - 2) + lines[7], flush=True)
     def hLine(self, x: int, y: int, lx: int, lines: str='───'):
+        """Makes a Horizontal line"""
         self.write(x, y, lines[0] + lines[1] * (lx - 2) + lines[2], flush=True)
     def vLine(self, x: int, y: int, ly: int, lines: str='│'):
+        """Makes a Vertical line"""
         for i in range(int(ly - 1)):
                 y += 1
                 self.DOWN()
                 self.write(x, y, lines[0], flush=True)
     def getch(self, no_echo=False) -> None:
+        """Input a Character from User"""
         if no_echo == False:
-            return getche()
+            ch = getch()
+            try:
+                self.write(None, None, ch.decode('utf-8'))
+            except:
+                return ''
+            return ch
         else:
             return getch()
     def clear(self) -> None:
+        """Clears the Screen"""
         print(chr(27)+'')
         print('\033c')
         print('\x1bc')
     def boxborder(self, lines: str='┌─┐││└─┘') -> None:
+        """Adds a Border around the Screen"""
         self.UP()
         self.box(0, 1, COLS, (LINES), lines)
-
